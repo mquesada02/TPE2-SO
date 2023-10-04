74eb98de3deb6bb5ab91f62a8019cdedc4657878
mquesada02 <mquesada@itba.edu.ar>
2023-10-04 16:10:26 -0300

    Setup fixes

--- setup.sh
+++ setup.sh
@@ -20,9 +20,18 @@ echo "Instalando PVStudio"
 echo ""
 sudo apt install pvs-studio
 
+cho "Agregando repositorio de git"
+sudo add-apt-repository ppa:git-core/ppa
+echo "Corriendo apt update"
+sudo apt update
+echo "Instalando git"
+sudo apt install git
+
 echo ""
 echo "Instalando los hooks"
 echo ""
 cp .hooks/* .git/hooks/
+
+git config core.hooksPath .hooks
 
 echo "Done!"
